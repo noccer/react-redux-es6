00cb4810b15a4327cac1ff030123c8a5117bf4cd
Niall O'Callaghan <noccer@gmail.com>
2017-05-11 16:12:15 +1000

    Update Entry Point.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router'; //browserHistory uses HTML pushState() which is not compatible with really old browsers
+import route from './routes';
+import './styles/styles.css'; // Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render (
+    <Router history={browserHistory} routes={routes} />, document.getElementById('app')
+);
