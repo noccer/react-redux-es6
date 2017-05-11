d3db3c5a318231b04c79af411cabbbfff60a6ae9
Niall O'Callaghan <noccer@gmail.com>
2017-05-11 17:13:16 +1000

    Fixed a typo.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router'; //browserHistory uses HTML pushState() which is not compatible with really old browsers
-import route from './routes';
+import routes from './routes';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
