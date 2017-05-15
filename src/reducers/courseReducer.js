8dce4bc5ce9a67c6878464e9efcc148979721651
Niall O'Callaghan <niall@rotageek.com>
2017-05-15 15:15:55 +1000

    Create courseReducer.

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,12 @@
+export default function courseReducer(state = [], action) { // take the current state, and action, then return a new state
+    switch(action.type) {
+        case 'CREATE_COURSE': 
+            return [
+                ...state,
+                Object.assign({}, action.course)
+            ];
+        
+        default: // in case the reducer does not have an appropriate action
+            return state;
+    }
+}
