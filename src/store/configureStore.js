f5f53f2cd5a19b4a8a443b857cb92b4a8b3da3cf
Niall O'Callaghan <niall@rotageek.com>
2017-05-15 16:36:45 +1000

    Add store.

---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+// this file ensures the app is configured when it starts up.
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) { //configureStore() will be used at the entry point of our application
+    return createStore( 
+        rootReducer,
+        initialState,
+        applyMiddleware(reduxImmutableStateInvariant()) // check out react slingshot on github to learn how to configure other middleware
+    );
+}
+
