4ae1b5bc3a1a7e4f39aaeb295e3f9b579cae4ed1
Niall O'Callaghan <niall@rotageek.com>
2017-05-16 10:59:48 +1000

    Load courses in reducer.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,11 +2,8 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) { // take the current state, and action, then return a new state. state = [] is initialising our state
     switch(action.type) {
-        case types.CREATE_COURSE: 
-            return [
-                ...state,
-                Object.assign({}, action.course)
-            ];
+        case types.LOAD_COURSES_SUCCESS: 
+            return action.courses;
         
         default: // in case the reducer does not have an appropriate action
             return state;
