8079cb188a53450f5d47b6cbbdb5bbff314edd73
Niall O'Callaghan <niall@rotageek.com>
2017-05-16 10:52:38 +1000

    Correct typo.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import think from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) { //configureStore() will be used at the entry point of our application
     return createStore( 
