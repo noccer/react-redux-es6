d62f3f0035abd601f2ae6a3420a8f7ed74ffeda1
Niall O'Callaghan <niall@rotageek.com>
2017-05-16 17:39:15 +1000

    Create AJAX status reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
     // courses: courses -> we could have written it like this
     courses, // this is called the shorthand property name in ES6.
-    authors
+    authors,
+    ajaxCallsInProgress
 });
 
 export default rootReducer;
