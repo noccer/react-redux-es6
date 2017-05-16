bc1fc4ebe130f141ff21f7fb527bcf53c3562e9c
Niall O'Callaghan <niall@rotageek.com>
2017-05-16 15:00:18 +1000

    Handle creates and updates in reducer.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -5,6 +5,18 @@ export default function courseReducer(state = initialState.courses, action) { //
     switch(action.type) {
         case types.LOAD_COURSES_SUCCESS: 
             return action.courses;
+
+        case types.CREATE_COURSE_SUCCESS:
+            return [
+                ...state, // explodes all value inside the array. in this case it's just an array of courses
+                Object.assign({}, action.course)
+            ];
+
+        case types.UPDATE_COURSE_SUCCESS:
+            return [
+                ...state.filter(course => course.id !== action.course.id),
+                Object.assign({}, action.course)
+            ];
         
         default: // in case the reducer does not have an appropriate action
             return state;
