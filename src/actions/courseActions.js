299bf01315ae5698f1672f7c8748b6104da7525d
Niall O'Callaghan <niall@rotageek.com>
2017-05-16 14:53:03 +1000

    Create save course thunk.

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -8,12 +8,40 @@ export function loadCoursesSuccess(courses) { //convenience function that return
     };
 }
 
+export function createCourseSuccess(course) {
+    return {
+        type: types.CREATE_COURSE_SUCCESS,
+        course
+    };
+}
+
+export function updateCourseSuccess(course) {
+    return {
+        type: types.UPDATE_COURSE_SUCCESS,
+        course
+    };
+}
+
 export function loadCourses() {
     return function(dispatch) {
         return courseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
             throw(error);
+        });
+    };
+}
+
+export function saveCourse(course) {
+    // see https://app.pluralsight.com/player?course=react-redux-react-router-es6&author=cory-house&name=react-redux-react-router-es6-m10&clip=9&mode=live
+    //                              getState is not used in this example BUT...
+    //                              ...this is very useful if we want to extract
+    //                           ⤺  some state from the Redux store!
+    return function(dispatch, getState) {
+        return courseApi.saveCourse(course).then(course => {
+            course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
+        }).catch(error => {
+            throw (error);
         });
     };
 }
