c1422cfbafe9dafa3af427eb68633bc06a75197d
Niall O'Callaghan <niall@rotageek.com>
2017-05-16 10:57:47 +1000

    Create Load_Courses Thunk.

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) { //convenience function that returns an action
+export function loadCoursesSuccess(courses) { //convenience function that returns an action
     return {
-        type: types.CREATE_COURSE,
-        course
+        type: types.LOAD_COURSES_SUCCESS,
+        courses
+    }
+}
+
+export function loadCourses() {
+    return function(dispatch) {
+        return courseApi.getAllCourses().then(courses => {
+            dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw(error);
+        });
     }
 }
