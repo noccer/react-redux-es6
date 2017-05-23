fe8ae0ddfdc45b5f7897d3476b9f00003f3fe706
Niall O'Callaghan <noccer@gmail.com>
2017-05-24 00:58:19 +1000

    Create form submission loading indicator (Updated with correct code).

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { loadCourses } from './actions/courseActions';
 import { loadAuthors } from './actions/authorActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore(); // create an instance of our store. you can hydrate this function with existing state from localstorage or server in configureStore(existingState)
 
